12833ae53db307b58a54cc302c701d331c01e411
Ashnoor Kaur <Ashnoor.kaur@edu.sait.ca>
2025-05-26 00:48:24 -0600

    week-1 check

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      
+     <main>
+      <h1 className="text-center mx-auto text-4xl mt-15">CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="./week-2" className="text-blue-400 hover:underline text-center mx-auto">Link to the week-2 page</Link>
+      </p>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Ashnoor Kaur</h1>
+             <p>
+                <Link href="https://github.com/ashnoorkaur/cprg306-assignments" className="text-blue-400 hover:underline"> cprg306-assignments</Link>
+            </p>
+        </main>
+    )
+}
